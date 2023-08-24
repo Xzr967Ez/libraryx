9e16a82add80249d74c251239167c502ba158686
Xzr967Ez <xanmoy.dev@gmail.com>
2023-08-24 11:06:19 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      div,con
+        <div className="grid grid-cols-4 gap-4">
+          <div className="flex bg-gray-900 h-40">
+            book1
+          </div>
+          <div className="flex bg-gray-900 h-40">
+            book2
+          </div>
+          <div className="flex bg-gray-900 h-40">
+            book3
+          </div>
+        </div>
+
+
+ 
+    </main>
+  )
+}
